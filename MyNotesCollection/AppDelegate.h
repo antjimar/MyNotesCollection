9a3b5ac5b32c6f1a68ecee09181f79b615b18f0b
Antonio Jimenez <antonio.jimenez2@gmail.com>
2015-02-13 15:41:18 +0100

    Initial Commit

---
+++ MyNotesCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNotesCollection
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 13/2/15.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyNotesCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNotesCollection
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 13/2/15.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
